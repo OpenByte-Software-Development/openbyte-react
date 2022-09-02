c102e92c5c18ece8d04184a21dd67abc02a22c02
cikpak <simion.gorgos@gmail.com>
2022-09-02 23:37:54 +0300

    Fix cookie alert on desktop

--- components/CookiesConsent.js
+++ components/CookiesConsent.js
@@ -11,7 +11,7 @@ const CookiesConsent = () => {
       declineButtonClasses="bg-transparent text-white text-base rounded-full border-white border-3 py-3.5 px-8 font-extrabold lg:w-max"
       buttonClasses="bg-orange text-white text-base rounded-full border-orange border-3 py-3 px-8 font-extrabold lg:w-max"
       containerClasses="flex flex-col lg:flex-row items-center max-w-[1184px] mx-auto left-0 right-0 fixed lg:rounded-full bg-light-black text-white text-base font-bold lg:py-8 lg:px-14 p-6 lg:mb-6 z-50"
-      buttonWrapperClasses="flex lg:flex-row flex-col  gap-6 lg:mt-0 mt-4 w-full"
+      buttonWrapperClasses="flex lg:flex-row flex-col  gap-6 lg:mt-0 mt-4 w-full lg:w-auto"
     >
       We use cookies to enhance your browsing experience, serve personalized ads
       or content, and analyze our traffic. By clicking &quot;Accept All&quot;,
