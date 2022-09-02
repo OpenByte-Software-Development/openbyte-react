63b5afbb242362fc9aa9c9efb17950782350576c
cikpak <simion.gorgos@gmail.com>
2022-09-02 21:51:11 +0300

    Add under construction template

---
+++ pages/calculator.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+import UnderConstruction from "../components/UnderConstruction";
+
+const Calculator = () => {
+  return (
+    <Layout>
+      <UnderConstruction />
+    </Layout>
+  );
+};
+
+export default Calculator;
---
+++ pages/case-studies.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+import UnderConstruction from "../components/UnderConstruction";
+
+const CaseStudies = () => {
+  return (
+    <Layout>
+      <UnderConstruction />
+    </Layout>
+  );
+};
+
+export default CaseStudies;
